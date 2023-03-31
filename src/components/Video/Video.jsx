77ae1dbdbfa995bc25fd8a84ec77774d6ea62d2b
João <joaovictorpfr@gmail.com>
2023-03-31 20:56:14 -0300

    Add controls and videos

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export function Video() {
+
+  const videoRef = useRef(null);
+
+  function handleStart(){
+    videoRef.current.play();
+  }
+
   return (
     <div
       className="
@@ -12,7 +19,15 @@ export function Video() {
       "
       style={{ scrollSnapAlign: "start" }}
     >
-      <video src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"></video>
+      <video 
+      src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+      controls
+      loop
+      ref={videoRef}
+      onClick={handleStart}
+      >
+
+      </video>
     </div>
   );
 }
