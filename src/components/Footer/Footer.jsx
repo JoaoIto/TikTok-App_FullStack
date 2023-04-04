c9f7b2781041cf688b583af3d5cf8886e6fafd0f
João <joaovictorpfr@gmail.com>
2023-04-04 15:38:55 -0300

    base complete desc

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -52,6 +52,18 @@ export function Footer() {
           <p id="title">Titulo da musica</p>
         </div>
       </div>
+      <img
+        height="40"
+        src="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/vinil.png?alt=media&token=d174bbec-b930-41e4-ab50-af1ea81bb7da"
+        alt="Icon vinil"
+        className="
+        h-16
+        invert
+        absolute
+        right-5
+        bottom-20
+        "
+      />
     </div>
   );
 }
